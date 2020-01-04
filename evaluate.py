7e92e2b543d3f2a62bb94175fd324f7ac30310c6
Sasha <alexander.drozd@gmail.com>
2020-01-04 22:16:33 +0900

    dataset by name

--- evaluate.py
+++ evaluate.py
@@ -2,13 +2,14 @@ import numpy as np
 from vecto.embeddings import load_from_dir
 from vecto.benchmarks.analogy import Analogy
 from vecto.data import get_dataset_by_name
-
+from vecto.utils.data import save_json
 
 # TODO: take this from config or command line args
 # TODO: and dataset name or path
 
 path_emb = "/home/blackbird/Cloud/remote/berlin/data/NLP/embeddings/test/w2v_ref/brown_thrd24__SG_d128_w3_neg4_i10"
-
+name_dataset = "dummy_analogy"  # "BATS"
+path_target = "/tmp/temp_target"
 
 # TODO: move this to vecto result parsing
 def get_mean_reciprocal_rank(results):
@@ -22,11 +23,11 @@ def get_mean_accuracy(results):
 
 embeddings = load_from_dir(path_emb)
 embeddings.cache_normalized_copy()
-# ds = get_dataset_by_name("BATS")
-ds = get_dataset_by_name("dummy_analogy")
+ds = get_dataset_by_name(name_dataset)
 bench_analogy = Analogy()
 results = bench_analogy.run(embeddings, ds)
 
+# TODO: move summarization of results to a separate module
 summary = {}
 summary["mean_reciprocal_rank"] = get_mean_reciprocal_rank(results)
 summary["mean_accuracy"] = get_mean_accuracy(results)
