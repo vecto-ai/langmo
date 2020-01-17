c2696060f4a6485bfb56e2cc48aed26ffa98c71e
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-17 13:51:36 +0900

    dont' trim corpus

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ def load_corpus(path_corpus):
     print("creating vocab")
     vocab = vecto.vocabulary.create_from_file(path_corpus)
     corpus_ids = vecto.corpus.load_file_as_ids(path_corpus, vocab)
-    corpus_ids = corpus_ids.astype(np.int64)[:4500]
+    corpus_ids = corpus_ids.astype(np.int64)
     corpus_ids.shape
     print(len(corpus_ids), max(corpus_ids))
     corpus_ids = torch.tensor(corpus_ids)
