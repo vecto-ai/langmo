5d11e58f2abf2d853978c3a0f190842c8ed0eb59
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-17 10:09:41 +0900

    save metadata

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from timeit import default_timer as timer
 from matplotlib import pyplot as plt
 from model import Net
 from protonn.utils import get_time_str
+from protonn.utils import save_data_json
 import platform
 import torch
 import torch.nn.functional as F
@@ -27,6 +28,7 @@ else:
 params["path_results"] = os.path.join(path_results_base, f"{get_time_str()}_{hostname}")
 os.makedirs(params["path_results"], exist_ok=True)
 params["batch_size"] = 4
+params["loss_history"] = []
 
 pos_corpus = 0
 len_sequence = 12
@@ -45,10 +47,9 @@ def init_model(cnt_words):
     scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
 
 
-def make_snapshot(id_epoch, vocab):
+def make_snapshot(id_epoch, vocab, params):
     print(f"creating ep {id_epoch} snapshot")
-    # TODO: save model for resume training
-    # TODO: save training stats
+    save_data_json(params, os.path.join(params["path_results"], "metadata.json"))
     embeddings = WordEmbeddingsDense()
     embeddings.vocabulary = vocab
     embeddings.metadata["vocabulary"] = vocab.metadata
@@ -65,13 +66,10 @@ def make_snapshot(id_epoch, vocab):
                 'scheduler_state_dict': scheduler.state_dict()},
                 os.path.join(params["path_results"], name_snapshot, "model"))
 
-
 def train_epoch(corpus_ids, vocab):
     global pos_corpus
-    # global cnt_corpus_passes
     pos_corpus = 0
     losses_epoch = []
-    # for id_batch in range(cnt_batches_per_epoch):
     # TODO: iterate oven number of batches with non-sequential sampling
     if pos_corpus > corpus_ids.shape[0] - len_sequence - offset_negative - offset_negative_max_random_add:
         RuntimeError("training corpus too short")
@@ -104,28 +102,27 @@ def train_batch(corpus_ids):
 def main():
     vocab, corpus_ids = load_corpus()
     init_model(vocab.cnt_words)
-    make_snapshot(0, vocab)
-    loss_history = []
+    make_snapshot(0, vocab, params)
     print("training")
     time_start_training = timer()
     for id_epoch in range(cnt_epochs):
         time_start = timer()
         loss_epoch = train_epoch(corpus_ids, vocab)
-        loss_history.append(loss_epoch)
+        params["loss_history"].append(loss_epoch)
         time_end = timer()
         print(id_epoch,
-              f"loss: {loss_history[-1]:.4f}",
+              f"loss: {params['loss_history'][-1]:.4f}",
               f"lr: {optimizer.param_groups[0]['lr']:.5f}",
               f"time ep: {time_end - time_start:.3f}s",
               f"time total: {datetime.timedelta(seconds=(time_end - time_start_training))}",
               )
-        plt.plot(np.arange(len(loss_history)), loss_history)
+        plt.plot(np.arange(len(params["loss_history"])), params["loss_history"])
         plt.xlabel("iter")
         plt.ylabel("loss")
         plt.savefig(os.path.join(params["path_results"], "loss.pdf"))
         plt.clf()
         id_epoch += 1
-        make_snapshot(id_epoch, vocab)
+        make_snapshot(id_epoch, vocab, params)
         scheduler.step()
 
 
