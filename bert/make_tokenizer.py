197a14e38e977668bbde889e62febc247b0beb30
Sasha <alexander.drozd@gmail.com>
2020-03-06 16:41:33 +0900

    metadata

--- bert/make_tokenizer.py
+++ bert/make_tokenizer.py
@@ -1,29 +1,52 @@
 from pathlib import Path
-
+import os
+import argparse
 from tokenizers import ByteLevelBPETokenizer, CharBPETokenizer, BertWordPieceTokenizer
+from protonn.utils import save_data_json
 
 # path_corpus = "/home/blackbird/Cloud/remote/berlin/data/NLP/corpora/raw_texts/Eng/BNC"
 # path_corpus = "/work/data/NLP/corpora/raw_texts/Eng/BNC"
 # path_corpus = "/home/blackbird/Projects_heavy/NLP/langmo/data/sample"
-path_corpus = "/home/blackbird/Projects_heavy/NLP/langmo/data/wikitext-2"
-paths = [str(x) for x in Path(path_corpus).glob("**/*.txt")]
 
-# Initialize a tokenizer
-#tokenizer = ByteLevelBPETokenizer()
-tokenizer = CharBPETokenizer()
-#tokenizer = BertWordPieceTokenizer()
 
-# Customize training
-tokenizer.train(files=paths, vocab_size=30000, min_frequency=5, special_tokens=[
-    "<s>",
-    "<pad>",
-    "</s>",
-    "<unk>",
-    "<mask>",
-])
-# tokenizer.train(files=paths, vocab_size=10000, min_frequency=1, special_tokens=[])
+def main():
+    parser = argparse.ArgumentParser(description='Benchmark me up, Scotty!')
+    parser.add_argument("path_corpus")
+    parser.add_argument("--tokenizer")
+    parser.add_argument('--min_frequency', type=int, default=5)
+    parser.add_argument('--vocab_size', type=int, default=30000)
+    parser.add_argument('--path_out', type=str, default="out")
 
-# Save files to disk
-tokenizer.save(".", "bnc")
-r = tokenizer.encode("Hi, how do you do NoNeXistingTkn?")
-print(r.tokens)
+    args = parser.parse_args()
+
+    path_corpus = args.path_corpus
+    paths = [str(x) for x in Path(path_corpus).glob("**/*.txt")]
+
+    tokenizers = {"byteBPE": ByteLevelBPETokenizer,
+                  "charBPE": CharBPETokenizer,
+                  "BertWP": BertWordPieceTokenizer}
+
+    tokenizer = tokenizers[args.tokenizer]()
+
+    # Customize training
+    tokenizer.train(files=paths,
+                    vocab_size=args.vocab_size,
+                    min_frequency=args.min_frequency,
+                    special_tokens=["<s>",
+                                    "<pad>",
+                                    "</s>",
+                                    "<unk>",
+                                    "<mask>"])
+
+    path_out = Path(args.path_out)
+    path_out = path_out / f"{args.tokenizer}_vs_{args.vocab_size}_m{args.min_frequency}"
+    os.makedirs(path_out, exist_ok=True)
+    tokenizer.save(str(path_out), "t")
+    r = tokenizer.encode("Hi, how do you do NoNeXistingTkn?")
+    print(r.tokens)
+    metadata = {}
+    metadata.update(vars(args))
+    save_data_json(metadata, path_out / "metadata.json")
+
+if __name__ == "__main__":
+    main()
