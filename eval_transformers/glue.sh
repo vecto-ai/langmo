67617cdd9d85781acebb900ba4c082b7e2c88df4
Sasha <alexander.drozd@gmail.com>
2020-05-16 16:06:39 +0900

    eval with transformers library

---
+++ eval_transformers/glue.sh
@@ -0,0 +1,21 @@
+#export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
+export GLUE_DIR=/mnt/storage/data/NLP/datasets/glue_data/
+export TASK_NAME=MNLI
+
+python3 run_glue.py \
+  --model_type bert \
+  --model_name_or_path bert-base-cased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --data_dir $GLUE_DIR/$TASK_NAME \
+  --max_seq_length 128 \
+  --train_batch_size 32 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 3.0 \
+  --output_dir /tmp/$TASK_NAME \
+  --overwrite_output_dir \
+  --logging_steps 50 \
+  --save_steps 200 \
+  --num_cores=8 \
+  --only_log_master
