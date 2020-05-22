d1c8aee3d7c8dd6d00b4b96bc02f5d449167af69
Aleksandr Drozd <blackbird@kiev2.m.gsic.titech.ac.jp>
2020-05-22 15:17:12 +0900

    fix

--- eval_transformers/glue.sh
+++ eval_transformers/glue.sh
@@ -15,12 +15,12 @@ python3 run_glue.py \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
-  --data_dir $GLUE_DIR/$TASK_NAME/$DATE_WITH_TIME \
+  --data_dir $GLUE_DIR/$TASK_NAME \
   --max_seq_length 128 \
   --per_gpu_train_batch_size 64 \
   --learning_rate 3e-5 \
   --num_train_epochs 3.0 \
-  --output_dir /work.fs/alex/DL_outs/NLP/$TASK_NAME \
+  --output_dir /work.fs/alex/DL_outs/NLP/$TASK_NAME/$DATE_WITH_TIME \
   --overwrite_output_dir \
   --logging_steps 200 \
   --save_steps -1 \
