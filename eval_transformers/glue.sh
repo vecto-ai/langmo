4cb922991e59825c6d8de425e14932b1b1dda65b
Aleksandr Drozd <blackbird@kiev2.m.gsic.titech.ac.jp>
2020-05-22 15:15:08 +0900

    eval scripts

--- eval_transformers/glue.sh
+++ eval_transformers/glue.sh
@@ -1,6 +1,13 @@
 #export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
-export GLUE_DIR=/mnt/storage/data/NLP/datasets/glue_data/
+#export GLUE_DIR=/mnt/storage/data/NLP/datasets/glue_data/
+export GLUE_DIR=/work.fs/data/NLP/datasets/glue_data
 export TASK_NAME=MNLI
+
+
+# TODO: add timestamp
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` 
+  
+echo $DATE_WITH_TIME
 
 python3 run_glue.py \
   --model_type bert \
@@ -8,14 +15,14 @@ python3 run_glue.py \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
-  --data_dir $GLUE_DIR/$TASK_NAME \
+  --data_dir $GLUE_DIR/$TASK_NAME/$DATE_WITH_TIME \
   --max_seq_length 128 \
-  --train_batch_size 32 \
+  --per_gpu_train_batch_size 64 \
   --learning_rate 3e-5 \
   --num_train_epochs 3.0 \
-  --output_dir /tmp/$TASK_NAME \
+  --output_dir /work.fs/alex/DL_outs/NLP/$TASK_NAME \
   --overwrite_output_dir \
-  --logging_steps 50 \
-  --save_steps 200 \
+  --logging_steps 200 \
+  --save_steps -1 \
   --num_cores=8 \
   --only_log_master
