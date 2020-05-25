02f3ef791815c751faccd12aeb6b456746eaab17
Aleksandr Drozd <blackbird@kiev7.m.gsic.titech.ac.jp>
2020-05-25 15:21:13 +0900

    more logs

--- eval_transformers/glue.sh
+++ eval_transformers/glue.sh
@@ -2,9 +2,9 @@
 #export GLUE_DIR=/mnt/storage/data/NLP/datasets/glue_data/
 export GLUE_DIR=/work.fs/data/NLP/datasets/glue_data
 export TASK_NAME=MNLI
-#MODEL_NAME=albert-base-v1
+MODEL_NAME=albert-base-v2
 #bert_base_cased
-MODEL_NAME=distilbert-base-cased
+#MODEL_NAME=distilbert-base-cased
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` 
   
@@ -16,12 +16,14 @@ python3 run_glue.py \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
+  --evaluate_during_training \
   --data_dir $GLUE_DIR/$TASK_NAME \
   --max_seq_length 128 \
   --per_gpu_train_batch_size 64 \
   --learning_rate 3e-5 \
   --num_train_epochs 3.0 \
   --output_dir /work.fs/alex/DL_outs/NLP/$TASK_NAME/$MODEL_NAME/$DATE_WITH_TIME \
+  --logging_dir /work.fs/alex/DL_outs/NLP/$TASK_NAME/$MODEL_NAME/$DATE_WITH_TIME\log \
   --overwrite_output_dir \
   --logging_steps 200 \
   --save_steps -1 \
