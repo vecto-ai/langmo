ef5c8382be3e42279497cf76e10d82d0f07a80bb
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-12-03 22:49:08 +0900

    make it work

--- eval_transformers/sample/run.sh
+++ eval_transformers/sample/run.sh
@@ -1,2 +1,10 @@
-python3 run_pl_glue.py --output_dir=./out --do_train --do_predict --model_name_or_path=albert-base-v2 --task=mnli --max_seq_lenght=128 --gpus=4
+python3 run_pl_glue.py \
+	--output_dir=./out \
+	--data_dir="/work.fs/data/NLP/datasets/glue_data/MNLI" \
+	--do_train \
+	--do_predict \
+	--model_name_or_path=albert-base-v2 \
+	--task=mnli \
+	--max_seq_length=128 \
+	--gpus=4
 
