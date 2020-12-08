8f9665208adb6568c3320537d18bf441f1054e76
aca10027xu <aca10027xu@es3.abci.local>
2020-12-08 17:13:57 +0900

    do validation before training

--- langmo/benchmarks/NLI/__main__.py
+++ langmo/benchmarks/NLI/__main__.py
@@ -74,25 +74,25 @@ class PLModel(pl.LightningModule):
 
     def validation_epoch_end(self, outputs):
         # print(describe_var(outputs))
-        if not self.trainer.running_sanity_check:
-            for i, lst_split in enumerate(outputs):
-                pref = self.ds_prefixes[i]
-                loss = torch.stack([x['val_loss'] for x in lst_split]).mean()
-                acc = torch.stack([x['val_acc'] for x in lst_split]).mean()
-                metrics = {
-                    f"val_loss_{pref}": loss,
-                    f"val_acc_{pref}": acc,
-                }
-                if self.hparams["test"] and i==2:
-                    print(
-                        f"worker {hvd.rank()} of {hvd.size()}\n"
-                        f"\tvalidation end\n"
-                        f"\tdl id is {i}, acc is {acc}"
-                    ) 
-                # print(f"worker {hvd.rank()}", metrics_dict)
-                # self.logger.agg_and_log_metrics(metrics, step=self.current_epoch)
-                # for md in metrics_dict:
-                self.log_dict(metrics, sync_dist=True)
+        #if not self.trainer.running_sanity_check:
+        for i, lst_split in enumerate(outputs):
+            pref = self.ds_prefixes[i]
+            loss = torch.stack([x['val_loss'] for x in lst_split]).mean()
+            acc = torch.stack([x['val_acc'] for x in lst_split]).mean()
+            metrics = {
+                f"val_loss_{pref}": loss,
+                f"val_acc_{pref}": acc,
+            }
+            if self.hparams["test"] and i==2:
+                print(
+                    f"worker {hvd.rank()} of {hvd.size()}\n"
+                    f"\tvalidation end\n"
+                    f"\tdl id is {i}, acc is {acc}"
+                ) 
+            # print(f"worker {hvd.rank()}", metrics_dict)
+            # self.logger.agg_and_log_metrics(metrics, step=self.current_epoch)
+            # for md in metrics_dict:
+            self.log_dict(metrics, sync_dist=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
@@ -146,7 +146,7 @@ def main():
         params["cnt_epochs"] = 2
     trainer = pl.Trainer(
         gpus=1,
-        num_sanity_val_steps=0,
+        num_sanity_val_steps=-1,
         max_epochs=params["cnt_epochs"],
         distributed_backend="horovod",
         replace_sampler_ddp=False,
