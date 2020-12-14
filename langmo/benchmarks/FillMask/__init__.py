a921280b4be334c6863e1d85a3d8b00c641afaf4
Sasha <alexander.drozd@gmail.com>
2020-12-14 15:58:18 +0900

    proto's running

---
+++ langmo/benchmarks/FillMask/__init__.py
@@ -0,0 +1,53 @@
+from transformers import pipeline
+from vecto.benchmarks.base import Benchmark as BaseBenchmark
+import json
+
+
+# TODO: we don't know which is mask token
+def get_queries(mask_token):
+    queries = [
+        f"I like {mask_token} beer.",
+        f"I like cold {mask_token}.",
+        f"Cows produce {mask_token}.",
+    ]
+    return queries
+
+
+def fill_mask(model, tokenizer, query):
+    fill_mask = pipeline(
+        "fill-mask",
+        model=model,
+        tokenizer=tokenizer)
+    result = fill_mask(query)
+    return result
+
+
+class Benchmark(BaseBenchmark):
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def run(self):
+        # TODO: reuse this from some parent class code
+        # TODO: move specific metadata to separate method anywya
+        result = dict()
+        result["setup"] = dict()
+        result["setup"]["name"] = "fill_mask"
+        result["setup"]["desciption"] = "Fill masked tokens"
+        result["results"] = []
+        # TODO: get this from model config
+        for query in get_queries("[MASK]"):
+            predicted = fill_mask(self.model, self.tokenizer, query)
+            result["results"].append(predicted)
+        return result
+
+
+def main():
+    from transformers import AutoModelForMaskedLM
+    from transformers import AutoTokenizer
+    name_model = "albert-base-v2"
+    model = AutoModelForMaskedLM.from_pretrained(name_model)
+    tokenizer = AutoTokenizer.from_pretrained(name_model)
+    benchmark = Benchmark(model, tokenizer)
+    result = benchmark.run()
+    print(result)
