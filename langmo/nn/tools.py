a2107bcf08bfde7e0b728da5ad4735500002cbcd
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-12-14 13:20:37 +0900

    reinit model

---
+++ langmo/nn/tools.py
@@ -0,0 +1,16 @@
+import torch
+
+
+def reinit_tensor(tensor):
+    mean = tensor.mean().item()
+    std = tensor.std().item()
+    torch.nn.init.normal_(tensor, mean, std)
+
+
+def reinit_parameter(param):
+    if hasattr(param, 'weight'):
+        reinit_tensor(param.weight.data)
+
+
+def reinit_model(model):
+    model.apply(reinit_parameter)
