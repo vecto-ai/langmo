e6f37a179e29861e1abc1daad2ceb2bbb6c38e68
Emil VATAI <emil.vatai@gmail.com>
2020-12-24 17:38:49 +0900

    Add relative imports to NLI

--- langmo/benchmarks/NLI/__main__.py
+++ langmo/benchmarks/NLI/__main__.py
@@ -11,9 +11,6 @@ import torch
 # import platform
 import torch.nn.functional as F
 import transformers
-from langmo.checkpoint import CheckpointEveryNSteps
-from langmo.nn.utils import reinit_model
-from langmo.utils import get_unique_results_path, load_config
 # from protonn.utils import describe_var
 from protonn.utils import get_time_str
 from pytorch_lightning.loggers import WandbLogger
@@ -22,6 +19,9 @@ from transformers import AutoModelForSequenceClassification
 from transformers import logging as tr_logging
 from transformers.optimization import get_linear_schedule_with_warmup
 
+from ...checkpoint import CheckpointEveryNSteps
+from ...nn.utils import reinit_model
+from ...utils import get_unique_results_path, load_config
 from .data import NLIDataModule
 
 # import logging
