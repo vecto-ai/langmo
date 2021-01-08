d31d89ebc97dc3f698363f5ee584c8b665cf7016
aca10027xu <aca10027xu@es3.abci.local>
2021-01-09 08:35:49 +0900

    pretrain runscripts

---
+++ sub_pretrain.sh
@@ -0,0 +1,15 @@
+if git config --get user.email > /dev/null; then
+    EMAILARGS="-m be -M $(git config --get user.email)"
+else
+    echo "$0 WARNING: git email not set!"
+fi
+
+#if [[ $SGE_CLUSTER_NAME = "t3" ]]; then
+    # TSUBAME 3.0
+#    qsub -g tgc-ebdcrest run.sh
+#else
+    # probably TSUBAME-KFC/DL
+    # sbatch run_kfc.sh
+    # abci
+    qsub -g gcb50300 $EMAILARGS run_pretrain.sh
+#fi
