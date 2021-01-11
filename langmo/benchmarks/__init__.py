3551982ddfd257539440f540df8375ddc7e9d9c3
Emil Vatai <emil.vatai@gmail.com>
2021-01-11 01:34:19 +0000

    Resolve "schedule actuall eval run on ABCI"

---
+++ langmo/benchmarks/__init__.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import stat
+
+
+# TODO: allow user to provide paltform-specific headers
+# TODO: this will be probably done though protonn
+header_ABCI = (
+    "#!/bin/bash\n"
+    "#$ -cwd\n"
+    "#$ -l rt_F=1\n"
+    "#$ -l h_rt=02:00:00\n"
+    "#$ -N NLP\n"
+    "#$ -j y\n"
+    "#$ -o $JOB_NAME.o$JOB_ID\n\n"
+    "source /etc/profile.d/modules.sh\n"
+    "source /home/aca10027xu/projects/langmo/modules.sh\n\n"
+    "NUM_NODES=${NHOSTS}\n"
+    "NUM_GPUS_PER_NODE=4\n"
+    "NUM_GPUS_PER_SOCKET=$(expr ${NUM_GPUS_PER_NODE} / 2)\n"
+    "NUM_PROCS=$(expr ${NUM_NODES} \\* ${NUM_GPUS_PER_NODE})\n"
+    "MPIOPTS=\"-np ${NUM_PROCS} -map-by ppr:${NUM_GPUS_PER_NODE}:node -mca pml ob1 -mca btl ^openib -mca btl_tcp_if_include bond0\"\n"
+)
+
+
+def schedule_eval_run(path):
+    num_runs = 1  # TODO: move this to config
+    for _ in range(num_runs):
+        # TODO: move group name to header of jobfile
+        cmd_submit = "qsub -g gcb50300"
+        cmd = f"{cmd_submit} {path}"
+        print(f"@@@ scheduling: {cmd}")
+        os.system(cmd)
+    # TODO: make wandb offline when schedule massive runs
+
+
+def make_executable(path):
+    st = os.stat(path)
+    os.chmod(path, st.st_mode | stat.S_IEXEC)
+
+
+def create_config_file(path, path_config):
+    with open(path_config, "w") as file_config:
+        file_config.write(f"model_name: {path / 'hf'}\n")
+        file_config.write(f"path_results: {path / 'eval'}\n")
+        file_config.write(f"cnt_epochs: 10\n")
+
+
+def create_job_files(path):
+    path_config = path / "evalconfig.yaml"
+    path_jobscript = path / "evaljobscript.sh"
+    create_config_file(path, path_config)
+    # TODO: do we need to set execution rights?
+    with open(path_jobscript, "w") as file_jobscript:
+        # TODO: get platform-specific headers from config
+        # e.g. for ABCI the nodes, the groups etc
+        # (nonTODO:) expect langmo to be installed by user
+        # TODO: use "schedule all" API
+        file_jobscript.write(header_ABCI)
+        file_jobscript.write("mpirun ${MPIOPTS} \\\n\t")
+        cmd = f"{sys.executable} -m langmo.benchmarks.NLI {path_config}\n"
+        file_jobscript.write(cmd)
+    # TODO: this is not gonna scale to multiple benchmarks - use some defaults
+    schedule_eval_run(path_jobscript)
--- langmo/checkpoint.py
+++ langmo/checkpoint.py
@@ -1,18 +1,5 @@
-import pytorch_lightning as pl
 from pathlib import Path
-
-
-# TODO: allow user to provide paltform-specific headers
-# TODO: this will be probably done though protonn
-header_ABCI = (
-    "#!/bin/bash\n"
-    "#$ -cwd\n"
-    "#$ -l rt_F=1\n"
-    "#$ -l h_rt=04:00:00\n"
-    "#$ -N NLP\n"
-    "#$ -j y\n"
-    "#$ -o $JOB_NAME.o$JOB_ID\n"
-)
+import pytorch_lightning as pl
 
 
 class BaseNStepCallback(pl.Callback):
@@ -27,9 +14,11 @@ class BaseNStepCallback(pl.Callback):
     def get_path_destination(self, trainer):
         global_step = trainer.global_step
         epoch = trainer.current_epoch
-        path_destination = Path(trainer.model.hparams["path_results"]) / \
-            self.prefix / \
+        path_destination = (
+            Path(trainer.model.hparams["path_results"]) /
+            self.prefix /
             f"epoch{epoch}_step{global_step}"
+        )
         return path_destination
 
 
@@ -44,28 +33,11 @@ class CheckpointEveryNSteps(BaseNStepCallback):
             trainer.model.tokenizer.save_pretrained(path_hf)
 
 
+# TODO: this moves to CLI. but we can create something like .doit file here
 class ScheduleEval(BaseNStepCallback):
-    def create_job_files(self, path):
-        path_config = path / "evalconfig.yaml"
-        # TODO: do we need to set execution rights?
-        with open(path / "evaljobscript.sh", "w") as file_jobscript:
-            # TODO: get platform-specific headers from config
-            # e.g. for ABCI the nodes, the groups etc
-            # python version
-            # expect langmo to be installed by user
-            # TODO: use "schedule all" API
-            file_jobscript.write(header_ABCI)
-            file_jobscript.write(f"python3 -m langmo.benchmarks.NLI {path_config}\n")
-        # TODO: this is not gonna scale to multiple benchmarks - use some defaults
-        with open(path_config, "w") as file_jobscript:
-            file_jobscript.write(f"model_name: {path / 'hf'}\n")
-            file_jobscript.write(f"path_results: {path / 'eval'}\n")
-            file_jobscript.write(f"cnt_epochs: 10\n")
-        # TODO: schedule N runs
-        # TODO: make wandb offline when schedule massive runs
 
     def on_batch_end(self, trainer: pl.Trainer, _):
         global_step = trainer.global_step
         if global_step % self.save_step_frequency == 0:
             path_dst = self.get_path_destination(trainer)
-            self.create_job_files(path_dst)
+            print("DON'T SCHEDULE EVAL HERE, USE CLI")
