037757fa2a2335e5d0a549b707d7b88c2e37d90f
Aleksandr Drozd <alexander.drozd@gmail.com>
2021-01-29 13:35:01 +0900

    fix lr scheduler

--- langmo/pretraining/__main__.py
+++ langmo/pretraining/__main__.py
@@ -6,6 +6,7 @@ import torch
 import transformers
 from protonn.utils import save_data_json
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import LearningRateMonitor
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 from transformers import logging as tr_logging
 from transformers.optimization import get_linear_schedule_with_warmup
@@ -43,16 +44,31 @@ class PLModel(pl.LightningModule):
         result = self.forward(batch)
         # TODO: how about loss only / more loss for masked tokens?
         loss = result["loss"]
-        # assert not torch.isnan(loss).item(), "loss is nan, can't train"
-        if torch.isnan(loss):
-            return None
-        # loss_mlm = for MLM, with long ids self.fwd_mlm()
+        assert not torch.isnan(loss).item(), "loss is nan, can't train"
+        # if torch.isnan(loss):
+        #     print(">> loss is NaN\n")
+        #     return None
+        # # loss_mlm = for MLM, with long ids self.fwd_mlm()
         # loss_nsp = for NSP self.fwd_nsp()
         # use different forwards
         # loss = loss_mlm + loss_nsp # + other aux tasks
-        # print(f"loss: {loss.item()}")
+        # lr = self.trainer.optimizers[0].param_groups[0]["lr"]
+        # print(f"ep {self.current_epoch}, step {self.global_step}, loss: {loss.item()}, lr {lr}")
         self.log("loss", loss)
         return loss
+
+    def optimizer_step(self,
+                       current_epoch,
+                       batch_nb,
+                       optimizer,
+                       optimizer_idx,
+                       closure,
+                       on_tpu=False,
+                       using_native_amp=False,
+                       using_lbfgs=False):
+        optimizer.step(closure=closure)
+        for scheduler in self.trainer.lr_schedulers:
+            scheduler["scheduler"].step()
 
     def configure_optimizers(self):
         optimizer = transformers.optimization.AdamW(
@@ -62,9 +78,14 @@ class PLModel(pl.LightningModule):
         # TODO: get rough estimation of training steps here
         # maybe after first epoch is trained - reset iterators?
         scheduler = get_linear_schedule_with_warmup(
-            optimizer, num_warmup_steps=1000, num_training_steps=500000
+            optimizer, num_warmup_steps=1000, num_training_steps=200000
         )
-        return [[optimizer], [scheduler]]
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': scheduler,
+            # 'monitor': 'lr'
+        }
+        # return [[optimizer], [scheduler]]
 
     # def save_pretrained(self):
     #     file_name = "tmp"  # logdir + current snapshot name
@@ -106,6 +127,7 @@ def main():
     n_steps_checkpoint = 5000  # TODO: should this go to params?
     on_n_step_checkpoint = CheckpointEveryNSteps(n_steps_checkpoint)
     # scheudle_eval_callback = ScheduleEval(n_step)
+    # lr_monitor = LearningRateMonitor(logging_interval="step")
     trainer = pl.Trainer(
         default_root_dir=params["path_results"],
         weights_save_path=params["path_results"],
@@ -125,7 +147,7 @@ def main():
         # TODO: is this ok?
         # theirs samples do like you did
         # but there is special checkpoint_callback param too....
-        callbacks=[on_n_step_checkpoint],
+        callbacks=[on_n_step_checkpoint],  # , lr_monitor],
         checkpoint_callback=False,
         gradient_clip_val=1.0,
         # TODO: figure out what is this
