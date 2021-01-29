2e222ce0e024b3e281d48a08e123b89f448f1df7
aca10027xu <alexander.drozd@gmail.com>
2021-01-29 10:28:38 +0900

    don't fail on nan

--- langmo/pretraining/__main__.py
+++ langmo/pretraining/__main__.py
@@ -43,7 +43,9 @@ class PLModel(pl.LightningModule):
         result = self.forward(batch)
         # TODO: how about loss only / more loss for masked tokens?
         loss = result["loss"]
-        assert not torch.isnan(loss).item(), "loss is nan, can't train"
+        # assert not torch.isnan(loss).item(), "loss is nan, can't train"
+        if torch.isnan(loss):
+            return None
         # loss_mlm = for MLM, with long ids self.fwd_mlm()
         # loss_nsp = for NSP self.fwd_nsp()
         # use different forwards
