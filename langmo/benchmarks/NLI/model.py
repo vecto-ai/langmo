f41053419015f4ef7174febeac07cfca12bf95c5
Aleksandr Drozd <alexander.drozd@gmail.com>
2021-05-28 12:50:29 +0000

    Resolve "siamese again"

--- langmo/benchmarks/NLI/model.py
+++ langmo/benchmarks/NLI/model.py
@@ -3,26 +3,77 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Siamese(nn.Module):
-    def __init__(self, bottom, top):
+def get_hidden_size(model):
+    if hasattr(model, "pooler"):
+        if hasattr(model.pooler, "out_features"):
+            return model.pooler.out_features
+        if hasattr(model.pooler, "dense"):
+            return (model.pooler.dense.out_features)
+    raise RuntimeError("can't estimate encoder output size")
+
+
+class SiameseBase(nn.Module):
+    def __init__(self, bottom, top, freeze_bottom=True):
         super().__init__()
         self.bottom = bottom
+        # self.bottom.requires_grad = not freeze_bottom
         self.top = top
 
-    def forward(self, inputs):
-        left, right = inputs
+    def combine(self, u, v):
+        return torch.cat((u, v, torch.abs(u - v), u * v), 1)
+
+    def save_pretrained(self, path):
+        print("save poretrained not impolemented")
+
+
+class BertWithLSTM(nn.Module):
+    def __init__(self, net, freeze):
+        super().__init__()
+        self.net = net
+        if freeze:
+            for param in self.net.parameters():
+                param.requires_grad = False
+        size_hidden = get_hidden_size(self.net)
+        self.rnn = nn.LSTM(input_size=size_hidden,
+                           hidden_size=size_hidden,
+                           num_layers=2,
+                           batch_first=True,
+                           dropout=0,
+                           bidirectional=True)
+
+    def lstm_out_to_tensor(self, x):
+        return x[0][:, -1, :]
+
+    def forward(self, **x):
+        h = self.net(**x)["last_hidden_state"]
+        # [:, 0, :]
+        # ["pooler_output"]
+        h = self.rnn(h)
+        return self.lstm_out_to_tensor(h)
+
+
+class SiameseLSTM(SiameseBase):
+    def forward(self, left, right):
         self.bottom.hidden = None
         h_left = self.bottom(left)[-1]
         self.bottom.hidden = None
         h_right = self.bottom(right)[-1]
-        # TODO: add [optionally] product and difference to concat
-        cnc = torch.cat((h_left, h_right), 1)
+        cnc = self.combine(h_left, h_right)
         h = self.top(cnc)
         return h
 
 
+class Siamese(SiameseBase):
+    def forward(self, left, right):
+        h_left = self.bottom(** left)
+        h_right = self.bottom(** right)
+        combined = self.combine(h_left, h_right)
+        h = self.top(combined)
+        return {"logits": h}
+
+
 class TopMLP2(nn.Module):
-    def __init__(self, in_size=512, hidden_size=256, cnt_classes=3):
+    def __init__(self, in_size=512, hidden_size=768, cnt_classes=3):
         super().__init__()
         self.l1 = nn.Linear(in_size, hidden_size)
         self.l2 = nn.Linear(hidden_size, cnt_classes)
@@ -45,7 +96,6 @@ class LSTM_Encoder(nn.Module):
                            bidirectional=True)
         # self.decoder = nn.Linear(nhid, 2)
         self.init_weights(embs)
-        self.hidden = None
         self.embs.weight.requires_grad = False
 
     def init_weights(self, embs):
@@ -68,8 +118,9 @@ class LSTM_Encoder(nn.Module):
         return output
 
 
-class Net(Siamese):
-    def __init__(self, embs):
-        encoder = LSTM_Encoder(embs, 128, 2)
-        top = TopMLP2()
-        super().__init__(encoder, top)
+# TODO: call it something more self-descriptive
+# class Net(Siamese):
+#     def __init__(self, embs):
+#         encoder = LSTM_Encoder(embs, 128, 2)
+#         top = TopMLP2()
+#         super().__init__(encoder, top)
