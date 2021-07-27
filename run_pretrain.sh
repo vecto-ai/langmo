70413cddf1c6fa81213d0b0294eaa963ae8fcdbf
aca10027xu <alexander.drozd@gmail.com>
2021-07-27 22:37:55 +0900

    tokenizer parallelism to env

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -l rt_AF=1
-#$ -l h_rt=48:00:00
+#$ -l h_rt=24:00:00
 #$ -N Pretrain
 #$ -j y
 #$ -o $JOB_NAME.o$JOB_ID
@@ -14,6 +14,7 @@ NUM_NODES=${NHOSTS}
 NUM_GPUS_PER_NODE=8
 NUM_GPUS_PER_SOCKET=$(expr ${NUM_GPUS_PER_NODE} / 2)
 NUM_PROCS=$(expr ${NUM_NODES} \* ${NUM_GPUS_PER_NODE})
+TOKENIZERS_PARALLELISM=true
 
 MPIOPTS="-np ${NUM_PROCS} -map-by ppr:${NUM_GPUS_PER_NODE}:node -mca pml ob1 -mca btl ^openib -mca btl_tcp_if_include bond0"
 
@@ -21,6 +22,7 @@ MPIOPTS="-np ${NUM_PROCS} -map-by ppr:${NUM_GPUS_PER_NODE}:node -mca pml ob1 -mc
 
 mpirun ${MPIOPTS} \
     python3 -m langmo.pretraining pretrain.yaml
+
 
 # horovodrun -np 4 python3 main.py
 
