148ae3fd03a22d970ecdc7e296dd9cf085325fc2
Aleksandr Drozd <alexander.drozd@gmail.com>
2021-08-05 12:49:00 +0900

    remove old glue code

--- eval_transformers/debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-JOB_NAME=$(grep -e '#\$ -N' run.sh | sed -e 's/.*-N //')
-
-git pull \
-&& rm -rf $JOB_NAME.o* \
-&& qsub -g gcb50300 run.sh \
-&& until ls $JOB_NAME.o* 2>/dev/null; do sleep 1; echo -n .; done \
-&& tail -f $JOB_NAME.o*
