cf72f7a765cd550fc0055b88701b48941bc9b12b
Aleksandr Drozd <alexander.drozd@gmail.com>
2021-08-05 12:51:13 +0900

    more cleanup

--- pl/model.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModelForSequenceClassification, AutoConfig
-
-config = AutoConfig.from_pretrained(
-    "albert-base-v2", num_labels=3)
-# finetuning_task=data_args.task_name,
-# cache_dir=model_args.cache_dir)
-
-net = AutoModelForSequenceClassification.from_pretrained("albert-base-v2", config=config)
--- pl/sub.sh
+++
@@ -1,9 +0,0 @@
-#if [[ $SGE_CLUSTER_NAME = "t3" ]]; then
-    # TSUBAME 3.0
-#    qsub -g tgc-ebdcrest run.sh
-#else
-    # probably TSUBAME-KFC/DL
-    # sbatch run_kfc.sh
-    # abci
-    qsub -g gcb50300 run.sh
-#fi
