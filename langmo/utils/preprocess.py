7e10b6b478eeadb5b39482bdde4347e44adb1f85
Aleksandr Drozd <alexander.drozd@gmail.com>
2022-06-05 06:30:37 +0000

    Resolve "optimize preprocess"

--- langmo/utils/preprocess.py
+++ langmo/utils/preprocess.py
@@ -1,6 +1,6 @@
+import argparse
 import json
 import random
-import sys
 from pathlib import Path
 from timeit import default_timer as timer
 
@@ -8,28 +8,38 @@ import humanfriendly
 from kapral.corpus import Corpus
 from transformers import AutoTokenizer
 
+from .json_reader import JSONLDocIter, QualityFilter
+
 
 def main():
-    # TODO: use argparse
-    if len(sys.argv) < 4:
-        print(f"usage: {sys.argv[0]} tokenizer max_length path_corpus path_output_base")
-        print(f"output directories corresponding to tokemnizer and sequence length will be appended automatically")
-        return -1
-    name_tokenizer = sys.argv[1]
-    max_length = int(sys.argv[2])
-    path = sys.argv[3]
-    path_out = Path(sys.argv[4]) / name_tokenizer / str(max_length)
+    parser = argparse.ArgumentParser(description='Split texts into training sequences of token ids')
+    parser.add_argument('name_tokenizer', type=str, help='tokenizer name')
+    parser.add_argument('max_length', type=int, help='sequence length')
+    parser.add_argument('path', type=str, help='corpus path')
+    parser.add_argument('path_out', type=str, help='output path')
+    parser.add_argument('--max_samples', type=int, help='limit number of samples', default=-1)
+    args = parser.parse_args()
+    name_tokenizer = args.name_tokenizer
+    max_length = args.max_length
+    path = Path(args.path)
+    path_out = Path(args.path_out) / name_tokenizer / str(max_length)
     path_out.mkdir(parents=True, exist_ok=True)
     path_out = path_out / "tokenized.jsonl"
     time_start = timer()
     tokenizer = AutoTokenizer.from_pretrained(name_tokenizer)
     line_buffer = [tokenizer.cls_token_id]
-    cnt_lines_written = 0
+    cnt_samples_written = 0
     proba_shortening = 0.1
     allowed_underfill = 10
-    with open(path_out, "w") as f_out:
+    if "json" in str(next(path.iterdir())):
+        print("detected JSONL files")
+        dirty_doc_iter = JSONLDocIter(path)
+        doc_iter = QualityFilter().get_document_iterator(dirty_doc_iter)
+    else:
         corpus = Corpus(path)
-        for doc in corpus.get_document_iterator():
+        doc_iter = corpus.get_document_iterator()
+    with open(path_out, "w") as f_out:
+        for doc in doc_iter:
             sent_iter = doc.get_sentence_iterator()
             for line in sent_iter:
                 tokens = tokenizer(line,
@@ -46,11 +56,17 @@ def main():
                     serialized = json.dumps(line_buffer)
                     f_out.write(serialized)
                     f_out.write("\n")
-                    cnt_lines_written += 1
-                    if cnt_lines_written % 10000 == 0:
-                        print(f"{cnt_lines_written} lines saved, last line len={len(line_buffer)}")
+                    cnt_samples_written += 1
+                    if cnt_samples_written % 10000 == 0:
+                        print(f"{cnt_samples_written} lines saved")
+                        print(f"\tlast line len={len(line_buffer)}")
                         print(tokenizer.decode(line_buffer))
+                        print()
                     line_buffer = [tokenizer.cls_token_id]
+                if cnt_samples_written > args.max_samples and args.max_samples > 0:
+                    break
+            if cnt_samples_written > args.max_samples and args.max_samples > 0:
+                break
             if len(line_buffer) > 1:
                 line_buffer += [tokenizer.sep_token_id]
     time_elapsed = timer() - time_start
--- prepare_test_data.sh
+++ prepare_test_data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 mkdir -p ./data/tokenized
-# python3 -m langmo.utils.preprocess roberta-base 128 ./data/sense ./data/tokenized/sense
-python3 -m langmo.utils.preprocess roberta-base 30 ./data/sample ./data/tokenized/sample
+
+python3 -m langmo.utils.preprocess roberta-base 128 ./data/sense ./data/tokenized/sense
+
+#python3 -m langmo.utils.preprocess roberta-base 256 \
+#    /mnt/storage/Data/NLP/corpora/original/openwebtext2 \
+#    /mnt/scratch/tokenized/owt2
+# python3 -m pstats token.profile
