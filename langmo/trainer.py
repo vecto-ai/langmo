b631cbff938936d2b138be2e5849d350d7df842c
gpucce <g.puccetti92@gmail.com>
2022-06-05 23:47:50 +0900

    and fix the fix

--- langmo/trainer.py
+++ langmo/trainer.py
@@ -45,7 +45,7 @@ def get_trainer(params, cluster_env):
         # but there is special checkpoint_callback param too....
         callbacks=[lr_monitor, Monitor()],
         gradient_clip_val=params["gradient_clip_val"],
-        enable_progress_bar=True,
+        enable_progress_bar=False,
         enable_checkpointing=False,
         # TODO: figure out what is this
         track_grad_norm=1,
