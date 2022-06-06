33878ff7def2497d3b177368a158ed6169a7d82b
Aleksandr Drozd <alexander.drozd@gmail.com>
2022-06-06 05:43:03 +0000

    Resolve "option to not save snapshots"

--- langmo/benchmarks/__init__.py
+++ langmo/benchmarks/__init__.py
@@ -4,20 +4,23 @@ import stat
 # import sys
 
 # TODO: allow user to provide paltform-specific headers
-# TODO: this will be probably done though protonn
+# TODO: this will be probably done through protonn
 # TODO: make console logs go to the target dir
 header_ABCI = (
     "#!/bin/bash\n"
     "#$ -cwd\n"
     "#$ -l rt_AF=1\n"
     "#$ -l h_rt=00:20:00\n"
-    "#$ -N NLP\n"
+    "#$ -N auto-finetune\n"
     "#$ -j y\n"
-    "#$ -o $JOB_NAME.o$JOB_ID\n\n"
+    "#$ -o ./logs/$JOB_NAME.o$JOB_ID\n\n"
     "source /etc/profile.d/modules.sh\n"
     "source /home/aca10027xu/projects/langmo/modules.sh\n\n"
     "NUM_NODES=${NHOSTS}\n"
-    "NUM_GPUS_PER_NODE=8\n"
+    "export NUM_GPUS_PER_NODE=8\n"
+    "export WANDB_MODE=offline\n"
+    "export TOKENIZERS_PARALLELISM=true\n"
+    "export PL_TORCH_DISTRIBUTED_BACKEND=NCCL\n"
     "NUM_GPUS_PER_SOCKET=$(expr ${NUM_GPUS_PER_NODE} / 2)\n"
     "NUM_PROCS=$(expr ${NUM_NODES} \\* ${NUM_GPUS_PER_NODE})\n"
     "MPIOPTS=\"-np ${NUM_PROCS} -map-by ppr:${NUM_GPUS_PER_NODE}:node -mca pml ob1 -mca btl ^openib -mca btl_tcp_if_include bond0\"\n"
@@ -45,8 +48,9 @@ def create_config_file(path, path_config):
         file_config.write(f"model_name: {path / 'hf'}\n")
         file_config.write(f"path_results: {path / 'eval'}\n")
         file_config.write(f"suffix: auto\n")
-        file_config.write(f"cnt_epochs: 4\n")
+        file_config.write(f"cnt_epochs: 8\n")
         file_config.write(f"batch_size: 128\n")
+        file_config.write(f"per_epoch_snapshot: false\n")
 
 
 def create_job_files(path):
@@ -61,6 +65,10 @@ def create_job_files(path):
         # TODO: use "schedule all" API
         file_jobscript.write(header_ABCI)
         file_jobscript.write("mpirun ${MPIOPTS} \\\n\t")
+        file_jobscript.write("-x TOKENIZERS_PARALLELISM \\\n\t")
+        file_jobscript.write("-x NUM_GPUS_PER_NODE \\\n\t")
+        file_jobscript.write("-x PL_TORCH_DISTRIBUTED_BACKEND \\\n\t")
+        file_jobscript.write("-x WANDB_MODE \\\n\t")
         # TODO: this failes if python on login node is different
         # cmd = f"{sys.executable} -m langmo.benchmarks.NLI {path_config}\n"
         cmd = f"python3 -m langmo.benchmarks.NLI {path_config}\n"
