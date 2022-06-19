98e127a9b26ad73a1d7c693aa75580ed139c016c
Emil VATAI <emil.vatai@gmail.com>
2022-06-19 19:43:40 +0900

    Better `pjsub` params

--- fugaku_pretrain.sh
+++ fugaku_pretrain.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/bash
-#PJM -L "rscunit=rscunit_ft01,rscgrp=small"
-#PJM -L elapse=08:00:00
-#PJM -L "node=8"
-#PJM --mpi "proc=32"
+#PJM -L "rscgrp=small"
+#PJM -L elapse=8:00:00
+#PJM -L "node=64"
+#PJM --mpi "proc=64"
 #PJM -j
 #PJM -g ra000012
 #PJM -S
+#PJM -o out-files/%n.%j.stdout
+#PJM -e out-files/%n.%j.stderr
+#PJM --spath out-files/%n.%j
 #PJM -x PJM_LLIO_GFSCACHE=/vol0004
 ##PJM --llio sharedtmp-size=80Gi
+##PJM -L "rscunit=rscunit_ft01,rscgrp=small"
 
-source ~/ra000012/venv/bin/activate
+source /home/ra000012/data/pytorch110.Kfast/venv/bin/activate
 
-# WANDB_MODE=offline
-WANDB_MODE=disabled \
-# mpirun -x LD_PRELOAD=libtcmalloc.so python3 -m langmo.pretraining pretrain.yaml
-mpirun -x LD_PRELOAD=libtcmalloc.so python3 -m langmo.NLI pretrain_minimal_test.yaml
+export WANDB_MODE="disbled"
+
+OMP_NUM_THREADS=48 mpirun \
+    -x WANDB_MODE \
+    python3 -m langmo.pretraining pretrain.yaml
 
