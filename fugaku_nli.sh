66bf49088862a6936c2e581a75284a8772b99101
Emil VATAI <emil.vatai@gmail.com>
2022-06-20 12:06:05 +0900

    Update `fugaku_pretrain.sh`

--- fugaku_nli.sh
+++ fugaku_nli.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
-#PJM -L "rscunit=rscunit_ft01,rscgrp=small"
+#PJM -L "rscgrp=small"
 #PJM -L elapse=08:00:00
 #PJM -L "node=16"
 #PJM --mpi "proc=16"
 #PJM -j
 #PJM -g ra000012
 #PJM -S
+#PJM -o out-files/%n.%j.stdout
+#PJM -e out-files/%n.%j.stderr
+#PJM --spath out-files/%n.%j.stat
 #PJM -x PJM_LLIO_GFSCACHE=/vol0004
 ##PJM --llio sharedtmp-size=80Gi
 
@@ -14,8 +17,6 @@ source /home/ra000012/data/pytorch110.Kfast/venv/bin/activate
 export NUM_GPUS_PER_NODE=0
 export TOKENIZERS_PARALLELISM=true
 export PL_TORCH_DISTRIBUTED_BACKEND=MPI
-
-# ======== Main ===========
 
 OMP_NUM_THREADS=48 mpirun \
     -x TOKENIZERS_PARALLELISM \
--- fugaku_pretrain.sh
+++ fugaku_pretrain.sh
@@ -8,16 +8,21 @@
 #PJM -S
 #PJM -o out-files/%n.%j.stdout
 #PJM -e out-files/%n.%j.stderr
-#PJM --spath out-files/%n.%j
+#PJM --spath out-files/%n.%j.stat
 #PJM -x PJM_LLIO_GFSCACHE=/vol0004
 ##PJM --llio sharedtmp-size=80Gi
-##PJM -L "rscunit=rscunit_ft01,rscgrp=small"
 
 source /home/ra000012/data/pytorch110.Kfast/venv/bin/activate
 
-export WANDB_MODE="disbled"
+export NUM_GPUS_PER_NODE=0
+export TOKENIZERS_PARALLELISM=true
+export PL_TORCH_DISTRIBUTED_BACKEND=MPI
+export WANDB_MODE="disabled"
 
 OMP_NUM_THREADS=48 mpirun \
+    -x TOKENIZERS_PARALLELISM \
+    -x NUM_GPUS_PER_NODE \
+    -x PL_TORCH_DISTRIBUTED_BACKEND \
     -x WANDB_MODE \
     python3 -m langmo.pretraining pretrain.yaml
 
