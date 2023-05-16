630329304438d71a92e4cf20b0c8ee6ec706e49c
Aleksandr Drozd <alexander.drozd@gmail.com>
2023-05-16 00:20:42 +0000

    Resolve "more predicton details"

--- fu_prepare_venv.sh
+++ fu_prepare_venv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-cp ~/opt/local-v1.13-langmo-mod.tgz /local
+cp ~/opt/local-v1.13-langmo-mod.tgz /local/
 cd /local
-tar -xf local-v1.13-langmo-mod.tgz
+pigz -dc local-v1.13-langmo-mod.tgz | tar xf -
 
 echo "setting up venv done"
